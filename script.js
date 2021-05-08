e943e6aa66dd75610478b4fffe2792d21009f3dc
niekurzakdaniel <niekurzakdaniel@gmail.com>
2021-05-08 07:21:28 +0200

    add set how many moves at once feature

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ const NEXT_MOVE="nextMove"
 const START_AUTO_GAME="startAutoGame"
 const STOP_AUTO_GAME="stopAutoGame"
 const RANDOM_MAP_FILL="randomMapFill"
+const HOW_MANY_MOVES_AT_ONCE_CONTIANER="howManyMovesAtOnceContainer"
+const HOW_MANY_MOVES_AT_ONCE_INPUT="howManyMovesAtOnceInput"
 
 const startGameBtn=document.getElementById(NEW_GAME)
 const resetGameBtn=document.getElementById(RESET_GAME)
@@ -21,9 +23,11 @@ const gameName = document.getElementById(GAME_NAME)
 const mapDimensions = document.getElementById(MAP_DIMENSIONS)
 const dimensionsContainer=document.getElementById(DIMENSIONS_CONTAINER)
 const randomMapFill=document.getElementById(RANDOM_MAP_FILL)
+const howManyMovesAtOnceContainer=document.getElementById(HOW_MANY_MOVES_AT_ONCE_CONTIANER)
+const howManyMovesAtOnceInput=document.getElementById(HOW_MANY_MOVES_AT_ONCE_INPUT)
 
 let mapDimensionsVal=0
-let internalId
+let intervalId
 let fieldsArr=[]
 
 const getDimensionsValueFromInput=()=>{
@@ -64,92 +68,95 @@ const mapGenerator=(random)=>{
             div.addEventListener('click',addRemoveCell)
             
             map.append(div)
-        }
-        
+        }   
     } 
 }
 
 const nextMove=()=>{ 
-    const newFieldsState=Array.from(Array(mapDimensionsVal), () => new Array(mapDimensionsVal).fill(false))
-  
-    const allFields=document.querySelectorAll(".field");
     
-    allFields.forEach(item=>{
-        if(item.classList.contains(CELL)){
-            item.classList.remove(CELL)
+    let howManyMoves=Number(howManyMovesAtOnceInput.value)
+   
+    for(let i=0;i<howManyMoves;++i){
+        const newFieldsState=Array.from(Array(mapDimensionsVal), () => new Array(mapDimensionsVal).fill(false))
+        const allFields=document.querySelectorAll(".field");
+    
+        allFields.forEach(item=>{
+            if(item.classList.contains(CELL)){
+                item.classList.remove(CELL)
+            }
+        })
+    
+        let neighborCount=0
+        for(let i=0;i<mapDimensionsVal;++i){
+            for(let j=0;j<mapDimensionsVal;++j){
+    
+                if(i-1>=0){
+                    if(fieldsArr[i-1][j]===true){
+                        neighborCount++;
+                    } 
+                    if(j-1>=0){
+                        if(fieldsArr[i-1][j-1]===true){
+                            neighborCount++;
+                        } 
+                    }
+                    if(j+1<mapDimensionsVal){
+                        if(fieldsArr[i-1][j+1]===true){
+                            neighborCount++;
+                        } 
+                    } 
+                }
+    
+                if(i+1<mapDimensionsVal){
+                    if(fieldsArr[i+1][j]===true){
+                        neighborCount++;
+                    } 
+                    if(j-1>=0){
+                        if(fieldsArr[i+1][j-1]===true){
+                            neighborCount++;
+                        } 
+                    }
+                    if(j+1<mapDimensionsVal){
+                        if(fieldsArr[i+1][j+1]===true){
+                            neighborCount++;
+                        } 
+                    } 
+                }
+    
+                if(j+1<mapDimensionsVal){
+                    if(fieldsArr[i][j+1]===true){
+                        neighborCount++;        
+                    }
+                }
+    
+                if(j-1>=0){
+                    if(fieldsArr[i][j-1]===true){
+                        neighborCount++;
+                    }
+                }
+    
+                if((neighborCount===3||neighborCount===2)&& fieldsArr[i][j]===true){
+                    newFieldsState[i][j]=true
+                }else if(neighborCount===3){
+                    newFieldsState[i][j]=true
+                }
+    
+                neighborCount=0
+            }    
         }
-    })
-
-    let neighborCount=0
-    for(let i=0;i<mapDimensionsVal;++i){
+        for(let i=0;i<mapDimensionsVal;++i){
+            for(let j=0;j<mapDimensionsVal;++j){
+                const fieldItem=document.querySelector(`[i="${i}"][j="${j}"]`)
+                if(newFieldsState[i][j]===true){
+                    fieldItem.classList.add(CELL)
+                }
+            }
+        }
+        if(invokeStopAutoGameIfNoChanges(fieldsArr,newFieldsState)){
+            return
+        }
+        fieldsArr=newFieldsState
         
-        for(let j=0;j<mapDimensionsVal;++j){
-
-            if(i-1>=0){
-                if(fieldsArr[i-1][j]===true){
-                    neighborCount++;
-                } 
-                if(j-1>=0){
-                    if(fieldsArr[i-1][j-1]===true){
-                        neighborCount++;
-                    } 
-                }
-                if(j+1<mapDimensionsVal){
-                    if(fieldsArr[i-1][j+1]===true){
-                        neighborCount++;
-                    } 
-                } 
-            }
-
-            if(i+1<mapDimensionsVal){
-                if(fieldsArr[i+1][j]===true){
-                    neighborCount++;
-                } 
-                if(j-1>=0){
-                    if(fieldsArr[i+1][j-1]===true){
-                        neighborCount++;
-                    } 
-                }
-                if(j+1<mapDimensionsVal){
-                    if(fieldsArr[i+1][j+1]===true){
-                        neighborCount++;
-                    } 
-                } 
-            }
-
-            if(j+1<mapDimensionsVal){
-                if(fieldsArr[i][j+1]===true){
-                    neighborCount++;        
-                }
-            }
-
-            if(j-1>=0){
-                if(fieldsArr[i][j-1]===true){
-                    neighborCount++;
-                }
-            }
-
-            if((neighborCount===3||neighborCount===2)&& fieldsArr[i][j]===true){
-                newFieldsState[i][j]=true
-            }else if(neighborCount===3){
-                newFieldsState[i][j]=true
-            }
-
-            neighborCount=0
-        }    
     }
-
-    for(let i=0;i<mapDimensionsVal;++i){
-        for(let j=0;j<mapDimensionsVal;++j){
-            const fieldItem=document.querySelector(`[i="${i}"][j="${j}"]`)
-            if(newFieldsState[i][j]===true){
-                fieldItem.classList.add(CELL)
-            }
-        }
-    }
-
-    invokeStopAutoGameIfNoChanges(fieldsArr,newFieldsState)
-    fieldsArr=newFieldsState
 }
 
 const invokeStopAutoGameIfNoChanges=(original,newArr)=>{
@@ -169,15 +176,15 @@ const newGame=()=>{
         map.style.visibility="visible"
         startAutoGameBtn.style.visibility="visible"
         stopAutoGameBtn.style.visibility="visible"
+        howManyMovesAtOnceContainer.style.visibility="visible"
         resetGameBtn.style.visibility="visible"
         nextMoveBtn.style.visibility="visible"
         startGameBtn.style.visibility="hidden"  
         randomMapFill.style.visibility="hidden"       
-        
+        howManyMovesAtOnceInput.value=1
         gameName.style.visibility="hidden"
         dimensionsContainer.style.visibility="hidden"
         fieldsArr=Array.from(Array(mapDimensionsVal), () => new Array(mapDimensionsVal).fill(false))
-
         const isRand=document.getElementById("randomInput").checked;
         mapGenerator(isRand)
     }
@@ -193,10 +200,12 @@ const resetGame=()=>{
     nextMoveBtn.style.visibility="hidden"
     startAutoGameBtn.style.visibility="hidden"
     stopAutoGameBtn.style.visibility="hidden"
+    howManyMovesAtOnceContainer.style.visibility="hidden"
     startGameBtn.style.visibility="visible"
     gameName.style.visibility="visible"
     randomMapFill.style.visibility="visible" 
     dimensionsContainer.style.visibility="visible"
+    clearInterval(intervalId);
 }
 
 const startAutoGame=()=>{
